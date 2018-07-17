6de26cef57ea564398f5011ddc034d8c29a04585
jimczj <jimczj@gmail.com>
2018-07-17 14:42:23 +0800

    style:修复h5样式问题

--- src/pages/more/more.js
+++ src/pages/more/more.js
@@ -15,89 +15,76 @@ export default class More extends Component {
   config = {
     navigationBarTitleText: '我的'
   }
-  constructor() {
-    super(...arguments)
-  }
-  componentWillMount () { }
-
-  componentDidMount () { }
-
-  componentWillUnmout () { }
-
-  componentDidShow () { }
-
-  componentDidHide () { }
-
   render () {
     return (
-        <View className='more'>
-          <View className='user flex-wrp'>
-            <View className='avatar flex-item'>
-              <Image className='userinfo-avatar' src='https://image.ibb.co/nKV5Cy/default_avatar.jpg' backgroundSize='cover'></Image>
+      <View className='more'>
+        <View className='user flex-wrp'>
+          <View className='avatar flex-item'>
+            <Image className='userinfo-avatar' src='https://image.ibb.co/nKV5Cy/default_avatar.jpg' backgroundSize='cover'></Image>
+          </View>
+          <View className='user-info flex-item'>
+            <Text className='userinfo-nickname'>阿集</Text>
+            <Text className='edit'>查看或编辑个人主页</Text>
+          </View>
+        </View>
+        <View className='my'>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={eyePng}></Image>
             </View>
-            <View className='user-info flex-item'>
-              <Text className='userinfo-nickname'>阿集</Text>
-              <Text className='edit'>查看或编辑个人主页</Text>
+            <View className='myitem-name flex-item'>
+              <Text>我的关注</Text>
             </View>
           </View>
-          <View className='my'>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={eyePng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的关注</Text>
-              </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={starPng}></Image>
             </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={starPng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的收藏</Text>
-              </View>
-            </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={draftPng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的草稿</Text>
-              </View>
-            </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={recentPng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>最近浏览</Text>
-              </View>
-            </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={bookPng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的书架</Text>
-              </View>
-            </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={livePng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的 Live</Text>
-              </View>
-            </View>
-            <View className='my-item flex-wrp'>
-              <View className='myitem-icon flex-item' >
-              <Image class='myitem-img' src={zhiPng}></Image>
-              </View>
-              <View className='myitem-name flex-item'>
-                <Text>我的值乎</Text>
-              </View>
+            <View className='myitem-name flex-item'>
+              <Text>我的收藏</Text>
             </View>
           </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={draftPng}></Image>
+            </View>
+            <View className='myitem-name flex-item'>
+              <Text>我的草稿</Text>
+            </View>
+          </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={recentPng}></Image>
+            </View>
+            <View className='myitem-name flex-item'>
+              <Text>最近浏览</Text>
+            </View>
+          </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={bookPng}></Image>
+            </View>
+            <View className='myitem-name flex-item'>
+              <Text>我的书架</Text>
+            </View>
+          </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={livePng}></Image>
+            </View>
+            <View className='myitem-name flex-item'>
+              <Text>我的 Live</Text>
+            </View>
+          </View>
+          <View className='my-item flex-wrp'>
+            <View className='myitem-icon flex-item' >
+            <Image class='myitem-img' src={zhiPng}></Image>
+            </View>
+            <View className='myitem-name flex-item'>
+              <Text>我的值乎</Text>
+            </View>
+          </View>
+        </View>
       </View>
     )
   }
